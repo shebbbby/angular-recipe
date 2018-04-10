32dc06f2689a1f9db4fec396efd1bd537cec4b2f
Daniel Shebshayevich <danielshebshayevich@daniels-imac.attlocal.net>
2018-04-10 19:29:05 -0400

    done

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import{ Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    
+
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
